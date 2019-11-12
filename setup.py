01245086bd314df750b62af638af12910bea36da
sscullen <ss.cullen@uleth.ca>
2019-11-12 15:20:58 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='spatial_ops',
+      version='0.0.1',
+      description='Utilities for downloading Landsat and Sentinel products from USGS',
+      url='https://github.com/sscullen/landsat_downloader.git',
+      author='Shaun Cullen',
+      author_email='ss.cullen@uleth.ca',
+      license='MIT',
+      packages=['spatial_ops'],
+      zip_safe=False)

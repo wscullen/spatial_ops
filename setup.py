301bf574d3e5725016488e9c109a087e72d3a6cb
Shaun Cullen <shaun@cullen.io>
2020-09-15 03:34:34 -0600

    fixed setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(name='spatial_ops',
       version='0.0.2',
-      description='Utilities for downloading Landsat and Sentinel products from USGS',
-      url='https://github.com/sscullen/landsat_downloader.git',
+      description='Utilities for working with shapefiles relating to L8 and S2 products.',
+      url='https://github.com/sscullen/spatial_ops.git',
       author='Shaun Cullen',
-      author_email='ss.cullen@uleth.ca',
+      author_email='shaun@cullen.io',
       license='MIT',
       packages=['spatial_ops'],
       zip_safe=False)

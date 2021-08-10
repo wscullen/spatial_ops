40759c6c92d342f74573ed343b4468db521be927
Shaun Cullen <shaun@cullen.io>
2021-08-09 22:37:55 -0600

    Created geojson geom export and github workflow.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
-setup(name='spatial_ops',
-      version='0.0.2',
-      description='Utilities for working with shapefiles relating to L8 and S2 products.',
-      url='https://github.com/sscullen/spatial_ops.git',
-      author='Shaun Cullen',
-      author_email='shaun@cullen.io',
-      license='MIT',
-      packages=['spatial_ops'],
-      zip_safe=False)
+__version__ = "v1.0.0"
+
+setup(
+    name="spatial_ops",
+    version=__version__,
+    description="Utilities for working with shapefiles relating to L8 and S2 products.",
+    url="https://github.com/sscullen/spatial_ops.git",
+    author="Shaun Cullen",
+    author_email="shaun@cullen.io",
+    license="MIT",
+    packages=["spatial_ops"],
+    zip_safe=False,
+)
